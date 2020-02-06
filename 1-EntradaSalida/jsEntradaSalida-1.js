9a18927f3b629c875b24f08af081deff7e5262ba
Matias-Castro022 <60743830+Matias-Castro022@users.noreply.github.com>
2020-02-06 11:13:27 -0300

    ejercicio uno
    La base mas baja de programar.

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,15 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+	var primerPrecio
+	var segundoPrecio
+	var apellido="nadie"
+
+	apellido="perez";
+	// es un valor literal si esta entre ""
+	//alert("tal cual");//literal
+	alert(apellido);
+	alert("apellido");
+	alert();//sale vacio
 }
 
