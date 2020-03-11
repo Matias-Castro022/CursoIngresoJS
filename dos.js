573de00f2f14b9e2a23183b9a92859dc70d733cb
Matias-Castro022 <60743830+Matias-Castro022@users.noreply.github.com>
2020-03-11 05:34:59 -0300

    Add files via upload

--- dos.js
+++ dos.js
@@ -1,94 +1,79 @@
-/*
-b)Para la gestión de un hotel,
-ingresar los siguientes datos validados de una reserva
-nombre del huésped
-cantidad de personas 
-cantidad de dia de estadia 
-forma de pago(efectivo , tarjeta o QR)
-
-informar el huésped que trajo más personas en una sola reserva.
-la cantidad de personas que se quedaron más días
-la forma de pago más utilizada.
-el promedio de cantidad de días por reserva
-*/
 function mostrar()
 {
-	var huesped;
-  	var cantidadPersonas;
-  	var maximoPersonas;
-  	var minimoPersonas;
-	var dias;
-	var masDias;
-	var medioDePago;
-	var medioMasUsado;
-	var respuesta;
+	var tipoDeIngrediente;
+	var cantidadKilos;
+	var kilosVegetal;
+	var kilosAnimal;
+	var kilosMixtos;
+	var pesoTotal;
+	var precioPorKilo;
+	var descuento;
+	var precioBruto;
+	var precioNeto;
+	var tipoDeIngredienteMasCaro;
+	var precioPorKiloTotal;
+	var promedioPrecioPorKilo;
 	var contador;
+	var respuesta;
 
 	contador=0;
-	respuesta='si';
+	respuesta="si";
+	bandera=0;
+	pesoTotal=0;
+	descuento=0;
+	precioPorKiloTotal=0;
 
-  	while(respuesta=="si")
-  	{
-  		contador=contador+1;
+	while(respuesta=="si")
+	{
+		tipoDeIngrediente=prompt("Qué tipo de ingrediente lleva?(vegetal; animal; mixto)");
+		while(!isNaN(tipoDeIngrediente)||tipoDeIngrediente!="vegetal" && tipoDeIngrediente!="animal" && tipoDeIngrediente!="mixto")
+		{
+			tipoDeIngrediente=prompt("Tipo de ingrediente invalido");
+		}
+		cantidadKilos=prompt("Cúantos kilos lleva?(entre 10 y 1000)");
+		cantidadKilos=parseInt(cantidadKilos);
+		while(isNaN(cantidadKilos)||cantidadKilos<10||cantidadKilos>1000)
+		{
+			cantidadKilos=prompt("Cantidad invalida.");
+			cantidadKilos=parseInt(cantidadKilos);
+		}
+		precioPorKilo=prompt("Ingrese el precio por kilo.");
+		precioPorKilo=parseInt(precioPorKilo);
+		while(isNaN(precioPorKilo)||precioPorKilo<1)
+		{
+			precioPorKilo=prompt("Precio invalido.");
+			precioPorKilo=parseInt(precioPorKilo);
+		}
+		precioBruto=pesoTotal*precioPorKilo;
+		console.log("El precio bruto es de "+precioBruto);
+		pesoTotal=pesoTotal+cantidadKilos;
+		precioPorKiloTotal=precioPorKiloTotal+precioPorKilo;
+		
+		contador=contador+1;
+		respuesta=prompt("Desea realizar otra compra?");
+	}
+	if (pesoTotal>99)
+	{
+		descuento=15;
+	}
+	if (pesoTotal>299)
+	{
+		descuento=25;
+	}
+	
+	precioNeto=precioBruto-precioBruto*descuento/100;
+	console.log("El precio neto es de "+precioNeto);
 
-  		huesped=prompt("Ingrese su nombre.");
-  		cantidadPersonas=prompt("Ingrese número de alojantes");
-  		cantidadPersonas=parseInt(cantidadPersonas);
-  		dias=prompt("Ingrese el número de dias que desea alojarse");
-  		dias=parseInt(dias);
-  		medioDePago=prompt("Ingrese su medio de pago(Efectivo/Tarjeta/QR)");
-
-  		while(!isNaN(huesped))
-  		{
-  			huesped=prompt("Nombre invalido");
-  		}
- 		while(isNaN(cantidadPersonas))
- 		{
- 			cantidadPersonas=prompt("número invalido.");
- 			cantidadPersonas=parseInt(cantidadPersonas);
- 		}
- 		while(isNaN(dias))
- 		{
- 			dias=prompt("cantidad de días invalido.");
- 			dias=parseInt(dias);
- 		}
- 		while(medioDePago!="efectivo" && medioDePago!="tarjeta" && medioDePago!="qr")
- 		{
- 			medioDePago=prompt("Medio de pago invalido");
- 		}
-
- 		respuesta=prompt("Desea agregar a un nuevo huesped.");
-
- 		if (contador==1)
- 		{
- 			minimoPersonas=cantidadPersonas;
- 			maximoPersonas=cantidadPersonas;
- 		}
- 		else
- 		{
- 			if (cantidadPersonas>maximoPersonas)
- 			{
- 				maximoPersonas=cantidadPersonas;
- 			}
- 			else
- 			{
- 				/*if (cantidadPersonas<minimoPersonas)
- 				{*/
- 				minimoPersonas=cantidadPersonas;
- 				//}
- 			}
- 		}
-
- 		
- 		
- 	}
-
- 	
- 	//console.log(huesped+" trajo un total de "+maximoPersonas+" personas");
-  	
-
-	document.write( <br>);
-	document.write( <br>);
-	document.write( <br>);
-	document.write( <br>);
-}	
+	promedioPrecioPorKilo=precioPorKiloTotal/contador;
+	console.log("el promedio de precio por kilo es de "+promedioPrecioPorKilo);
+}
+/*compra de ingredientes por mayor, hasta que el cliente quiera.
+tipo("v"; "a"; "m")(vegetal; animal; mixto).
+peso(entre 10 y 1000).
+precio por kilo(mayor a 0).
+si se compra mas de 100 kilos tiene un 15% de descuento.
+si se compra mas de 300 kilos tiene un 25% de descuento.
+a	importe total a pagar.
+b	importe con descuento.
+c	el tipo de alimento mas caro.
+d	el promedio de precio por kilo en total.*/
--- uno.js
+++ uno.js
@@ -1,90 +1,104 @@
 
 function mostrar()
 {
-	var nombre;
-	var edad;
-	var menores;
-	var adolecentes;
-	var adultos;
-	var terciarios;
-	var sexo;
-	var hombres;
-	var mujeres;
-	var temperatura;
+	var tipoDeProducto;
+	var cantidadDeProducto;
+	var cantidadDeBarbijos;
+	var cantidadDeJabones;
+	var cantidadDeAlcoholes;
+	var marca;
+	var fabricante;
+	var PrecioBarbijoMasCaro;
+	var precioBarbijoMasCaroCantidad;
+	var precioBarbijoMasCaroFabricante;
+	var itemMasUnidades;
+	var itemMasUnidadesFabricante;
 	var contador;
-	var acumulador;
-	var respuesta;
+	var bandera;
 
-	nombre=prompt("ingrese nombre.");
-	edad=prompt	("ingrese edad.");
-	edad=parseInt(edad);
-	sexo=prompt("ingrese f ó m.");
-	temperatura=prompt("ingrese temperatura.");
-	temperatura=parseInt(temperatura);
-
-	menores=0
-	adolecentes=0
-	mayores=0
-	terciarios=0
-	hombres=0;
-	mujeres=0;
 	contador=0;
-	acumulador=0;
-	respuesta='si';
-	
-	while(respuesta=="si")
+	bandera=0;
+	cantidadDeJabones=0;
+	itemMasUnidades=0;
+
+	while(contador<5)
 	{
-		contador=contador+1;
-		edad=prompt	("ingrese edad.");
-		while(isNaN(edad) || (edad<0 && edad>110))
+		tipoDeProducto=prompt("Qué producto necesita?(barbijo/jabon/alcohol)");
+		while(!isNaN(tipoDeProducto)||tipoDeProducto!="barbijo"&&tipoDeProducto!="jabon"&&tipoDeProducto!="alcohol")
 		{
-			edad=prompt("Edad incorrecta. reingrese edad.");
-			edad=parseInt(edad);
-			if(edad>0 && edad<13)
-			{
-				menores=menores+1;
-			}
-			else
-			{
-				if (edad>12 && edad<18)
-				{
-					adolecentes=adolecentes+1;
-				}
-				else
-				{
-					if(edad>17 && edad<61)
-					{
-						mayores=mayores+1;
-					}
-					else
-					{
-						terciarios=terciarios+1;
-					}
-				}	
-			}
+			tipoDeProducto=prompt("Producto invalido.");
 		}
-		while(sexo!="f" || sexo!="m")
+		precio=prompt("Indique el precio(Entre 100 y 300)");
+		precio=parseInt(precio);
+		while(isNaN(precio)|| precio<100 || precio>300)
 		{
-			sexo=prompt("sexo invalido. reingrese.");
-			if (sexo=="f")
-			{
-				mujeres=mujeres+1;
-			}
-			else
-			{
-				hombres=hombres+1;
-			}
+			precio=prompt("Precio invalido");
+			precio=parseInt(precio);
 		}
-		while(temperatura<29 || temperatura>42)
+		cantidadDeProducto=prompt("Cúantas unidades requiere?(entre 1 y 1000)");
+		cantidadDeProducto=parseInt(cantidadDeProducto);
+		while(isNaN(cantidadDeProducto)||cantidadDeProducto<1||cantidadDeProducto>1000)
 		{
-			temperatura=prompt("estas en riesgo o reingrese	temperatura.");
-			temperatura=parseInt(temperatura);
+			cantidadDeProducto=prompt("Cantidad invalida.");
+			cantidadDeProducto=parseInt(cantidadDeProducto);
 		}
-		respuesta=prompt("ingrese si para continuar.");
+		marca=prompt("Qué marca solicita?(A/B)");
+		while(!isNaN(marca)||marca!="a" && marca!="b")
+		{
+			marca=prompt("Marca invalida.");
+		}
+		fabricante=prompt("Qué fabricante solicita?(lala ó lele)");
+		while(!isNaN(fabricante)||fabricante!="lala" && fabricante!="lele")
+		{
+			fabricante=prompt("Fabricante invalido.");
+		}
+
+		if (tipoDeProducto=="barbijo" && bandera==0|| PrecioBarbijoMasCaro<precio)
+		{
+			PrecioBarbijoMasCaro=precio;
+			//console.log(PrecioBarbijoMasCaro);
+			precioBarbijoMasCaroFabricante=fabricante;
+			//console.log(precioBarbijoMasCaroFabricante);
+			precioBarbijoMasCaroCantidad=cantidadDeProducto;
+			//console.log(precioBarbijoMasCaroCantidad);
+
+			bandera=bandera+1;
+		}
+		if (contador==0)
+		{
+			itemMasUnidades=cantidadDeProducto;
+		}
+		else 
+		{
+			if (cantidadDeProducto>itemMasUnidades)
+			{
+				itemMasUnidades=tipoDeProducto;
+				itemMasUnidadesFabricante=fabricante;
+			}
+		}	
+		if (tipoDeProducto=="jabon")
+		{
+			cantidadDeJabones=cantidadDeJabones+cantidadDeProducto;
+		}
+
+		contador=contador+1
 	}
-		/*if()
-		{
-
-		}*/
-
+	alert("los barbijos mas caros son del fabricante "+precioBarbijoMasCaroFabricante+" y hay un total de "+precioBarbijoMasCaroCantidad+" barbijos");
+	console.log("el barbijo mas caro es del fabricante "+precioBarbijoMasCaroFabricante+" y hay un total de "+precioBarbijoMasCaroCantidad+" barbijos");
+	alert("el item con mas unidades es "+itemMasUnidades+" del fabricante "+fabricante);
+	console.log("el item con mas unidades es "+itemMasUnidades+" del fabricante "+fabricante);
+	alert("Hay un total de "+cantidadDeJabones+" jabones.");
+	console.log("Hay un total de "+cantidadDeJabones+" jabones.");
 }
+/*realizar 5 cargas de productos de prevencion de contagio.
+validar los siguientes datos:
+tipo(validar "barbijo", "jabon" y "alcohol").
+precio(entre 100 y 300).
+cantidad de unidades(mayor a 0 y menor a 1000).
+marca.
+fabricante.
+Se debe informar lo siguiente:
+A	Del mas caro de los barbijos, la cantidad de unidades y
+el fabricante.
+B	Del item con mas unidades, el fabricante.
+C	Cuantas unidades de jabon hay en total.*/
